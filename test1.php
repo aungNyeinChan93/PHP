012e78ec974ff2a6bf4401a0ad26a6f86d8aa8da
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2024-12-28 22:09:43 +0630

    d_2

--- test1.php
+++ test1.php
@@ -19,18 +19,26 @@
     </style>
 </head>
 
-
-
 <!-- php -->
 <?php
 $users = ['mgmg', 'aung', 'susu'];
 
 $books = [
-    ['name' => 'book one', 'year' => '2000'],
-    ['name' => 'book two', 'year' => '1999'],
-    ['name' => 'book three', 'year' => '1970'],
-    ['name' => 'book four', 'year' => '2021'],
+    ['name' => 'book one', 'year' => '2000', 'author' => 'john'],
+    ['name' => 'book two', 'year' => '1999', 'author' => 'doe'],
+    ['name' => 'book three', 'year' => '1970', 'author' => 'boo'],
+    ['name' => 'book four', 'year' => '2021', 'author' => 'foo'],
 ];
+
+function flterByAuthor(array $books): array
+{
+    $authors = array_map(function ($book) {
+        return $book['author'];
+    }, $books);
+    return $authors;
+}
+
+$authors = flterByAuthor($books);  //as like computed
 
 function filterByYear(array $books, string $year = 'null'): array
 {
@@ -46,6 +54,61 @@ function userName(array $params): string
 {
     return $params[0];
 }
+
+// general filter
+$filter = function (array $items, $key, $value): mixed {
+    $filterItems = [];
+    foreach ($items as $item) {
+        if ($item[$key] === $value) {
+            return $filterItems = $item;
+        }
+    }
+    return $filterItems;
+};
+
+$years = $filter($books, "year", '2000');
+$writers = $filter($books, "author", 'foo');
+
+// general map
+$map = function ($array, $key, $value = null) {
+    $mapArray = array_map(function ($i) use ($key, $value) {
+        if ($value) {
+            return $i[$key] == $value;
+        } else {
+            return $i[$key];
+        }
+    }, $array);
+    return $mapArray;
+};
+
+$myAuthorName = $map($books, "author");
+$myByAuthorName = $map($books, "author", 'doe'); //return true and false
+
+var_dump($myAuthorName);
+var_dump($myByAuthorName);
+
+
+// props with fun 
+$filterbyfn = function ($array, $fn) {
+    $items = [];
+    foreach ($array as $value) {
+        if ($fn($value)) {
+            $items[] = $value;
+        }
+    }
+    return $items;
+};
+$fiterFn = $filterbyfn($books, function ($book) {
+    return $book['year'] == '2000';
+});
+var_dump($fiterFn);
+
+// build in filter
+$buildInFilter = array_filter($books, function ($book) {
+    return $book['author'] == 'foo';
+});
+print_r($buildInFilter);
+
 
 
 class Test
@@ -84,11 +147,7 @@ class Test
 
 $test_one = new Test('aung aung');
 
-
 ?>
-
-
-
 
 
 <body class="">
@@ -110,11 +169,9 @@ $test_one = new Test('aung aung');
     </div>
 
     <?= userName($users) ?>
-
     <?= $test_one->callName() ?>
     <?= $test_one::test() ?>
     <?= $test_one->all()[1] ?>
-
 
     <hr>
     <h3>Function</h3>
@@ -132,13 +189,56 @@ $test_one = new Test('aung aung');
         </div>
     </div>
 
+    <hr>
+    <div class="row mx-5 my-2">
+        <div class="col-3">
+            <div class="card p-2">
+                <ul class="list-group">
+                    <?php foreach ($authors as $author): ?>
+                        <li class="list-group-item"> <?= $author ?> </li>
+                    <?php endforeach ?>
+                </ul>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card p-2">
+                <ul class="list-group">
+                    <?php foreach ($years as $year): ?>
+                        <li class="list-group-item"> <?= $year ?> </li>
+                    <?php endforeach ?>
+                </ul>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card p-2">
+                <ul class="list-group">
+                    <?php foreach ($writers as $writer): ?>
+                        <li class="list-group-item"> <?= $writer ?> </li>
+                    <?php endforeach ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <hr>
+
 </body>
-
-
 
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+<script>
+
+    let hit = function () {
+        console.log('hit');
+    };
+    hit();
+
+
+
+
+</script>
 
 </html>
